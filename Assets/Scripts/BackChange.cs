8d4aa08411803311395c674c7ce6f8528c6c6e30
N0I0C0K <nick131410@aliyun.com>
2022-01-22 21:17:46 +0800

    v0.5版本

---
+++ Assets/Scripts/BackChange.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite whiteSprite;
+    public Sprite drakSprite;
+    public Transform cameraPos;
+    public Vector3 prePos;
+    private SpriteRenderer spriteRenderer;
+    public Vector3 offsetPos = new Vector3(0, 0, 0);
+    void Start()
+    {
+        if (whiteSprite == null || drakSprite == null)
+        {
+            Debug.LogError("drakSprite | whiteSprite error");
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        prePos = cameraPos.position;
+        MyGameManager.changeCurrentColor += this.onChangeColor;
+    }
+
+    void onChangeColor(Platform.PlatformColor color)
+    {
+        if (color == Platform.PlatformColor.WHITE)
+        {
+            this.spriteRenderer.sprite = whiteSprite;
+        }
+        else
+        {
+            this.spriteRenderer.sprite = drakSprite;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cpos = cameraPos.position;
+        offsetPos += (cpos - prePos) / 60;
+        prePos = cpos;
+        offsetPos.Set(offsetPos.x, offsetPos.y, 0);
+        this.transform.position = cpos + offsetPos + new Vector3(0, 0, 10);
+
+
+    }
+}
--- Assets/Scripts/Platform/TrapPlatform.cs
+++ Assets/Scripts/Platform/TrapPlatform.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TrapPlatform : MonoBehaviour
 {
-    [SerializeField] private bool isCanBeTouch;
+    [SerializeField] private bool isCanBeTouch = true;
     private SpriteRenderer spriteRenderer;
     private void Start()
     {
