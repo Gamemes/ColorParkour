4a0a8824d4ca7d31a0d9e641407160ed61d4eb53
N0I0C0K <nick131410@aliyun.com>
2022-01-22 14:43:41 +0800

    添加了movePlatform的预制体

--- Assets/Scripts/Player/PlayerDie.cs
+++ Assets/Scripts/Player/PlayerDie.cs
@@ -34,9 +34,5 @@ namespace Player
                 PlayerDying.Invoke();
             }
         }
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            Debug.Log("enter");
-        }
     }
 }
