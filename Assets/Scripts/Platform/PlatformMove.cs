2534f17d5aac29cf083f0b944f2c4f8d4bd9fbcb
N0I0C0K <nick131410@aliyun.com>
2022-01-22 14:57:04 +0800

    修复了移动平台

--- Assets/Scripts/Platform/PlatformMove.cs
+++ Assets/Scripts/Platform/PlatformMove.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
 
-public class PlatformMove : MonoBehaviour
+namespace Platform
 {
-    [SerializeField] private float MoveSpeed;               //平台移动速度
-    [SerializeField] private Transform[] wayPoints;         //移动点的数组
-    [SerializeField] private int index;                     //数组索引
-
-    private void Start()
+    public class PlatformMove : MonoBehaviour
     {
-        index = 0;
-    }
-
-    private void Update()
-    {
-        ///使物体向着目标点移动
-        transform.position = Vector2.MoveTowards(transform.position, wayPoints[index].position, MoveSpeed * Time.deltaTime);
-
-        ///当物体离目标点足够近时切换目标点
-        if (Vector2.Distance(transform.position,wayPoints[index].position)<= 0.05f)
+        [SerializeField] private float MoveSpeed;               //平台移动速度
+        [SerializeField] private Transform[] wayPoints;         //移动点的数组
+        [SerializeField] private int index;                     //数组索引
+        private bool isPlayerOnPlatform = false;
+        private Transform playerTransform;
+        private void Start()
         {
-            index++;
-            if(index >=  wayPoints.Length)                  //数组索引越界返回
+            index = 0;
+        }
+
+        private void Update()
+        {
+            ///使物体向着目标点移动
+            Vector2 tarPos = Vector2.MoveTowards(transform.position, wayPoints[index].position, MoveSpeed * Time.deltaTime);
+            if (isPlayerOnPlatform)
             {
-                index = 0;
+                Vector2 dpos = new Vector2(tarPos.x - transform.position.x, tarPos.y - transform.position.y);
+                playerTransform.position += new Vector3(dpos.x, dpos.y);
+            }
+            this.transform.position = tarPos;
+            ///当物体离目标点足够近时切换目标点
+            if (Vector2.Distance(transform.position, wayPoints[index].position) <= 0.05f)
+            {
+                index++;
+                if (index >= wayPoints.Length)                  //数组索引越界返回
+                {
+                    index = 0;
+                }
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("enter");
+            if (other.tag.Equals("Player"))
+            {
+                isPlayerOnPlatform = true;
+                playerTransform = other.transform;
+            }
+        }
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.tag.Equals("Player"))
+            {
+                isPlayerOnPlatform = false;
             }
         }
     }
